21d31395ca936efc571184e7775139c72b4fb220
Sassafras <jasoncfinley@gmail.com>
2021-02-25 13:07:33 -0600

    Intersection class

--- Qualifying2021Package/Intersection.java
+++ Qualifying2021Package/Intersection.java
@@ -1,5 +1,26 @@
 package Qualifying2021Package;
 
-public class Intersection {
+import java.util.ArrayList;
 
+public class Intersection {
+    private int id;
+    private ArrayList<String> entryStreets = new ArrayList<String>();
+    private ArrayList<String> exitStreets = new ArrayList<String>();
+    private ArrayList<Light> lights = new ArrayList<Light>();
+
+    public Intersection(int id) {
+        this.id = id;
+    }
+
+    public void addEntryStreet(String streetName) {
+        entryStreets.add(streetName);
+
+        //Light light = new Light(streetName);
+
+        //lights.add(light);
+    }
+
+    public void addExitStreet(String streetName) {
+        exitStreets.add(streetName);
+    }
 }
